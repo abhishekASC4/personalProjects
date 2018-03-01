dff22f4d0f9d7e3290b41166f69630c53b0bf9f4
unknown <abhishekramjit@ymail.com>
2018-03-01 18:50:50 -0500

    added likely final edit before redesign

--- personalSiteExample/code.js
+++ personalSiteExample/code.js
@@ -1,8 +1,10 @@
-var aboutMe = `<p class = 'contentText'>lorem ipsum dolor</p>`;
+var aboutMe = `<p class = 'contentText'>A New York based programmer</p>`;
 var contactMe = `
     <p class = 'contentText'>Check me out at: <br>
-    <img src = "assets/github.svg">
-    <img src = "assets/linkedin.svg"></p>
+    <a href = "https://github.com/abhishekASC4">
+    <img src = "assets/github.svg"></a>
+    <a href = "https://www.linkedin.com/in/abhishek-ramjit-b83335147/">
+    <img src = "assets/linkedin.svg"></p></a>
     `;
 var skills = `
     <p class = 'contentText'> Proficient in:<br>
@@ -10,7 +12,7 @@ var skills = `
     <img src = "assets/css3.svg"> <br>
     <img src = "assets/javascript.svg">
 `;
-var defaultContent = `<p class = 'contentText'> A New York based programmer <p>`
+var defaultContent = `<p class = "contentText"> Abhishek Ramjit <p>`
 function replaceContent(newContent){
     $('#content').replaceWith(` <div class="content bottomRow rhombus" id = "content">
         <div class= "dummy"></div>` + newContent + `</div>`);
@@ -43,9 +45,13 @@ function checkClicks(){
         $(replaceContent(skills));
         $(setUnderline('#skills'));
     })
-    $(" body > div:not(.main)").click(function(){
+    $('.notMain').click(function(){
         $(replaceContent(defaultContent));
-        console.log('out')
+        $('#content').css({
+            'background-color':'black',
+        })
+        $('.contentText').css('color', 'white');
+        $('.rhombus p ').css('text-decoration','none');
     })
 }
 $(document).ready(function(){
